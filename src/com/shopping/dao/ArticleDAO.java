3fad2897e7aa9b8b88248d24390c9e06352eeaf2
BoulmanHamza <hamzaboulman132@gmail.com>
2022-03-08 12:14:50 +0000

    addarticledao

--- src/com/shopping/dao/ArticleDAO.java
+++ src/com/shopping/dao/ArticleDAO.java
@@ -1,4 +1,13 @@
 package com.shopping.dao;
 
+import java.util.List;
+
+import com.shopping.model.article;
+
 public interface ArticleDAO {
+	public void create(article a);
+	public void delete(article a);
+	public article findById(int IdArticle);
+	public List<article> findAll();
+	public void update(article a);
 }
